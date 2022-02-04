1bcd65dd49fe1014364fe4bc9c9d62e1e5648c8e
obieuan <obieuan@gmail.com>
2022-02-03 22:52:42 -0600

    upload

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Esta funcion  imprime un texto con printf seguido de una línea
+ * Return: 1 always
+ */
+int main(void)
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
