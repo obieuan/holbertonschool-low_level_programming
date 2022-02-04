9652cfc4569516c212a60a4b6930fbfa3a62aef2
obieuan <obieuan@gmail.com>
2022-02-03 19:42:07 -0600

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,9 @@
 /**
  * main - Esta funcion  imprime el texto despues de puts
  */
+/**
+ * Return: 0 just because
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
