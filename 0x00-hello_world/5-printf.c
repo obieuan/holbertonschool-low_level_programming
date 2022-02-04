6a00167ed7c7c7933e2f47e16d91ba5e44a5e282
obieuan <obieuan@gmail.com>
2022-02-03 19:55:55 -0600

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - Esta funcion  imprime un texto con printf seguido de una línea 
+ * Return: 0 always
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
