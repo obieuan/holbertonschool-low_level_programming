a200f93cd1daeb8c62debfaade44fd2c3d84b489
obieuan <obieuan@gmail.com>
2022-02-08 23:18:03 -0600

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * add - This do stuff
+ * @a: primer numero
+ * @b: segundo numero
+ * Return: la suma
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
