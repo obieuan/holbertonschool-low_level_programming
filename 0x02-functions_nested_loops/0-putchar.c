034fffb27b0c0ba4636dc290f3c7b2af390e2151
obieuan <obieuan@gmail.com>
2022-02-08 18:23:09 -0600

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Esta funcion  imprime el texto _putchar
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i = 0;
+	char cadena[] = "_putchar";
+	int size = sizeof(cadena);
+
+	for (i = 0; i <= size; i++)
+	{
+		putchar(cadena[i]);
+	}
+	return (0);
+}
