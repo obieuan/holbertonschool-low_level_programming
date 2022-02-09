65ad345519552f345565a6db8f5321aa1c69a97c
obieuan <obieuan@gmail.com>
2022-02-08 22:45:09 -0600

    update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,34 +7,25 @@ void jack_bauer(void)
 {
 	int h = 0;
 	int m = 0;
+	int h1 = 0;
+	int h2 = 0;
+	int m1 = 0;
+	int m2 = 0;
 
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			char a = h + '0';
-			char b = m + '0';
-			
-			if (h < 10)
-			{
-				_putchar(48);
-				_putchar('0' + h);
-			}
-			else
-			{
-				_putchar(a);
-			}
-			_putchar(58);
-			if (m < 10)
-			{
-				_putchar(48);
-				_putchar('0' + m);
-			}
-			else
-			{
-				_putchar(b);
-			}
-			_putchar(10);
+			h1 = h / 10 + '0';
+			h2 = h % 10 + '0';
+			m1 = m / 10 + '0';
+			m2 = m % 10 + '0';
+			_putchar(h1);
+			_putchar(h2);
+			_putchar(':');
+			_putchar(m1);
+			_putchar(m2);
+			_putchar('\n');
 		}
 	}
 }
