b63c96248f51cbd545e028465e11528e3b1d368e
obieuan <obieuan@gmail.com>
2022-02-08 20:50:46 -0600

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Esta funcion  imprime el texto _putchar
+ * Return: 0 always
+ */
+void print_alphabet(void)
+{
+	int i = 97;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+}
