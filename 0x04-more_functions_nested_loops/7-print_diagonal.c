754a149436635fcc8c6fd63ab430a25f54db3376
obieuan <obieuan@gmail.com>
2022-02-10 21:52:21 -0600

    upload

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_diagonal - check the code
+ * @n: number received
+ * Return: multi
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('\\');
+		}
+	}
+	_putchar(10);
+}
