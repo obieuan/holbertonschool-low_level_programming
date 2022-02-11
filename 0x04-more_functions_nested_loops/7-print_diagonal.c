5ec074c7c4a5537359a53579d9eac1dd66e8bf7d
obieuan <obieuan@gmail.com>
2022-02-10 22:11:11 -0600

    upload

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,14 +7,23 @@
 void print_diagonal(int n)
 {
 	int i = 0;
+	int a = 0;
 
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
+			for (a = 0; a != n; a++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar(10);
 		}
-		_putchar(92);
 	}
-	_putchar(10);
+	else
+	{
+		_putchar(10);
+	}
+
 }
