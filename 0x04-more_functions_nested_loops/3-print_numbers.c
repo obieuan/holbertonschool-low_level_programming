c55d0d037942f274b12a22e1d504c24145c24611
obieuan <obieuan@gmail.com>
2022-02-10 20:45:52 -0600

    upload

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - check the code
+ * Return: multi
+ */
+
+void print_numbers(void)
+{
+	int a = 0;
+
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+	}
+	_putchar(10);
+}
