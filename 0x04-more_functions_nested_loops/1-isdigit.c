b9a41846172cdda59c126769a6d2e55e0df67281
obieuan <obieuan@gmail.com>
2022-02-10 20:30:03 -0600

    upload

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - check the code
+ * @c:variable que recibe
+ * Return: 1 if upper 0 if lower
+ */
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
