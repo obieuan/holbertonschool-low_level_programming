6cd479875345573e3a74abbad0dc3459cc36e247
obieuan <obieuan@gmail.com>
2022-02-10 22:42:42 -0600

    upload

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,22 +11,23 @@ int main(void)
 
 	for (i = 1; i < 101; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf("Fizz");
 		}
-		if (i % 5 == 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf("Buzz");
 		}
-		if ((i % 3 != 0) && (i % 5 != 0))
+		else if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
 		{
 			printf("%d", i);
 		}
-		if (i != 100)
-		{
-			printf(" ");
-		}
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
