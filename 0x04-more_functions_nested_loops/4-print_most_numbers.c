162040eb498ac845295b54dba8faea1665b74e0d
obieuan <obieuan@gmail.com>
2022-02-10 20:50:09 -0600

    upload

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - check the code
+ * Return: multi
+ */
+
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	for (a = 48; a < 58; a++)
+	{
+		if (a != 50 && a != 52)
+		{
+			_putchar(a);
+		}
+	}
+	_putchar(10);
+}
