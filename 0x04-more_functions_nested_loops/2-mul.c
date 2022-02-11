8e1888de5cf871408acc0026569a49c318a679e1
obieuan <obieuan@gmail.com>
2022-02-10 20:37:28 -0600

    upload

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - check the code
+ * @a: nuemro a multiplicar
+ * @b: segundo numero a multiplicar
+ * Return: multi
+ */
+
+int mul(int a, int b)
+{
+	int multi = 0;
+
+	multi = a * b;
+	return (multi);
+}
