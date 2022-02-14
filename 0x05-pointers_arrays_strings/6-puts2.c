18266d7c4df6ef13250712a0ab5ecf5c39cd90f9
obieuan <obieuan@gmail.com>
2022-02-14 16:42:29 -0600

    update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,14 +7,14 @@
 void puts2(char *str)
 {
 	int i;
-	int j = 0;
+	int c = 0;
 
-	while (str[j] != '\0')
+	while (str[c] != '\0')
 	{
-		j++;
+		c++;
 	}
 
-	for (i = 0; i < j; i += 2)
+	for (i = 0; i < c; i += 2)
 	{
 		_putchar(str[i]);
 	}
