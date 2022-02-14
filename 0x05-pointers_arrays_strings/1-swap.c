ef70965fcf741f8ce89c532c774b7f7d612352d7
obieuan <obieuan@gmail.com>
2022-02-14 15:11:30 -0600

    update

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - check the code
+ * @a: pointer 1
+ * @b: pointer 2
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
