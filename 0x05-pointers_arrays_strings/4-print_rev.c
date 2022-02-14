21292ed886235d3f324c6e03b71110d1cc5f739c
obieuan <obieuan@gmail.com>
2022-02-14 15:52:58 -0600

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - return size of strlen
+ * @s: pointer 1
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+
+	_putchar('\n');
+}
