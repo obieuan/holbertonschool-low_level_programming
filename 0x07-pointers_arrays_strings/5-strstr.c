e98a73a0a00aef55c728c0f4a8b4724dfb6b0d6b
obieuan <obieuan@gmail.com>
2022-02-21 19:12:40 -0600

    update

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,14 +1,31 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _memcpy - The _memcpy() function copies n bytes from memory
+ * _strstr - The _memcpy() function copies n bytes from memory
  * area src to memory area dest
- * @dest: pointer to string
- * @src: byte a llenar
- * @n: numero de espacios
+ * @haystack: pointer to string
+ * @needle: byte a llenar
  * Return: dest
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *_strstr(char *haystack, char *needle)
 {
+	int a;
+	char *p;
+
+	if (*needle == '\0')
+		return (haystack);
+	for (; *haystack != '\0'; haystack++)
+	{
+		a = 0;
+		p = haystack;
+		while (*p == needle[a])
+		{
+			a++;
+			p++;
+			if (needle[a] == '\0')
+				return (haystack);
+		}
+	}
+	return (NULL);
 
 }
