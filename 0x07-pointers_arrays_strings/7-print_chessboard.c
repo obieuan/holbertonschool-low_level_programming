174611502d03d99bd2d8e6bec274befc3295bdae
obieuan <obieuan@gmail.com>
2022-02-21 19:03:52 -0600

    update

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _memcpy - The _memcpy() function copies n bytes from memory
+ * area src to memory area dest
+ * @dest: pointer to string
+ * @src: byte a llenar
+ * @n: numero de espacios
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+}
