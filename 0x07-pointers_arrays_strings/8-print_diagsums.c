6fb6b00724b09658fa11991436425703afc3bb7e
obieuan <obieuan@gmail.com>
2022-02-21 19:18:19 -0600

    update

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,14 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _memcpy - The _memcpy() function copies n bytes from memory
+ * print_diagsums - The _memcpy() function copies n bytes from memory
  * area src to memory area dest
- * @dest: pointer to string
- * @src: byte a llenar
- * @n: numero de espacios
+ * @a: pointer to string
+ * @size: byte a llenar
  * Return: dest
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+void print_diagsums(int *a, int size)
 {
+	int i, j, p, l = 0, r = 0;
 
+	for (i = 0; i < size; i++)
+	{
+		p = (i * size) + i;
+		l += *(a + p);
+	}
+	for (j = 0; j < size; j++)
+	{
+		p = (j * size) + (size - 1 - j);
+		r += *(a + p);
+	}
+	printf("%i, %i\n", l, r);
 }
