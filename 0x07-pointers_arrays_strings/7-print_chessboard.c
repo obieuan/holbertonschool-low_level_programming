3d33c5c0b3d9df437b3f34ec020a6f2005e33c89
obieuan <obieuan@gmail.com>
2022-02-21 19:15:34 -0600

    update

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,14 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _memcpy - The _memcpy() function copies n bytes from memory
+ * print_chessboard - The _memcpy() function copies n bytes from memory
  * area src to memory area dest
- * @dest: pointer to string
- * @src: byte a llenar
- * @n: numero de espacios
- * Return: dest
+ * @a: pointer to string
+ * Return: nothing
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+void print_chessboard(char (*a)[8])
 {
+	int b, c;
 
+	for (b = 0; b < 8; b++)
+	{
+		for (c = 0; c < 8; c++)
+		{
+			_putchar(a[b][c]);
+		}
+		_putchar('\n');
+	}
 }
