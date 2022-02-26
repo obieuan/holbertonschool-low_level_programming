4906a8fb30ac88c190f6ff6f1f35467b52392220
obieuan <obieuan@gmail.com>
2022-02-25 18:44:56 -0600

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: - argc
+ * @argv: - argv *
+ * Return: Always 0.
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
