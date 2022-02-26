19c2b960397399268cab3c9dffe3a72f3e3310ab
obieuan <obieuan@gmail.com>
2022-02-25 18:38:40 -0600

    update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: - argc
+ * @argv: - argv *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
