94c4b8a4e435bd32da6f54dd342b603ec5003c56
obieuan <obieuan@gmail.com>
2022-03-07 23:32:22 -0600

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct dog - list dates about pet
+ * @name: pet name
+ * @age: pet age
+ * @owner: pet owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
