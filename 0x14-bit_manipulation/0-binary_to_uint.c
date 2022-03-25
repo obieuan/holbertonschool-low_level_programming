d71edcac195ecb82aa36c7833b5219436d388ac1
obieuan <obieuan@gmail.com>
2022-03-24 21:23:45 -0600

    update

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * binary_to_uint - convierte numero binario a int sin signo
+ * @b: puntero a string de 0 y 1 chars
+ * Return: numero convertido, o 0 si hay error
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int num;
+
+	num = 0;
+	if (!b)
+    {
+        return (0);
+    }
+		
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+        {
+            return (0);
+        }			
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+        {
+            num += 1;
+        }			
+	}
+	return (num);
+}
