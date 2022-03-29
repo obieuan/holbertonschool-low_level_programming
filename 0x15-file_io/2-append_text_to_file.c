9163407fdcd182995b632d32100c370613b788e5
obieuan <obieuan@gmail.com>
2022-03-28 18:39:54 -0600

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include "main.h"
 /**
  * create_file - read the code
@@ -11,27 +9,26 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int ID = 0, numChar = 0, cantidad = 0;
+	int i = 0, texto;
 
 	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	texto = open(filename, O_WRONLY | O_APPEND);
+
+	if (texto == -1)
 	{
 		return (-1);
 	}
-	if (text_content != NULL)
-	{
-		for (cantidad = 0; text_content[cantidad] != '\0'; cantidad++)
-		{}
-	}
-	ID = open(filename, O_APPEND | O_WRONLY, 0600);
-	if (ID == -1)
-	{
-		return (-1);
-	}
-	numChar = write(ID, text_content, cantidad);
-	if (numChar == -1)
-	{
-		return (-1);
-	}
-	close(ID);
+	write(texto, text_content, i);
 	return (1);
 }
