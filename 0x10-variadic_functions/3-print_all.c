da35626ea569c91b0015b890378dc560c5c8178e
obieuan <obieuan@gmail.com>
2022-07-04 12:56:37 -0500

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,47 +1,88 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
+/**
+ * p_char - print char
+ * @list:arg
+ * Return: void
+ */
 
-void op_char(char)
+void p_char(va_list list)
 {
-	printf("%c", va_arg(pseudo, char));
+	printf("%c", va_arg(list, int));
 }
+/**
+ * p_string - print string
+ * @list:arg
+ * Return: void
+ */
 
-void po_int(int)
+void p_string(va_list list)
 {
-	printf("%d", va_arg(pseudo, int));
-}
+	char *str;
 
-void op_float(float)
+	str = va_arg(list, char*);
+	if (str == NULL)
+		str = "(nil)";
+printf("%s", str);
+}
+/**
+ * p_integer - print integer
+ * @list:arg
+ * Return: void
+ */
+
+void p_integer(va_list list)
 {
-	printf("%f", va_arg(pseudo, float));
+	printf("%i", va_arg(list, int));
 }
+/**
+ * p_float - print float
+ * @list:arg
+ * Return: void
+ */
 
-void op_string(char *)
+void p_float(va_list list)
 {
-	printf("%s", va_arg(pseudo, char *));
+	printf("%f", va_arg(list, double));
 }
-
+/**
+ * print_all - print everything
+ * @format:arg
+ * Return: void
+ */
 void print_all(const char * const format, ...)
-{
-	va_list pseudo;
-	va_start(pseudo, format);
 
-	op_t ops[] = {
-		{"c", op_char},
-		{"i", op_int},
-		{"f", op_float},
-		{"s", op_string},
+{
+	unsigned int i, j;
+	t_print t[] = {
+		{"c", p_char},
+		{"s", p_string},
+		{"i", p_integer},
+		{"f", p_float},
 		{NULL, NULL}
 	};
-	int i;
+	va_list valist;
+	char *s = "";
 
-
-	while()
-	while()
-		if()
-		if()
-		if()
-
+	va_start(valist, format);
+	i = 0;
+	while (format && format[i])
+	{
+		j = 0;
+		while (t[j].x != NULL)
+		{
+			if (*(t[j].x) == format[i])
+			{
+				printf("%s", s);
+				t[j].T_func(valist);
+				s = ", ";
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(valist);
 	printf("\n");
 }
